24fbd8ee424d3cb3a130ee224db7955bf9118fbb
Hutesh Vidhate <huteshvidhate23@gmail.com>
2025-05-09 21:27:12 +0530

    secnd commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Online_pharmacy_react_vite
+
 React vite website online pharmacy
+The project is pharma-react-app
+the second file is not complete
+
+just download the folder
+
+paste the command
+1]cd my-project
+2]npm install
+3]npm run dev
+
+project will run automatically
+if you weant see the website then the netlify deployed linkj is here:
+
+---
+
+:
--- pharma-react-app/src/firebase/firebaseConfig.js
+++ pharma-react-app/src/firebase/firebaseConfig.js
@@ -3,16 +3,24 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
+// const firebaseConfig = {
+//     apiKey: "AIzaSyDJ7YFne4QBWBcE45KUOmV8yIyHZP40rYI",
+//     authDomain: "onlinepharmacyreact.firebaseapp.com",
+//     databaseURL: "https://onlinepharmacyreact-default-rtdb.asia-southeast1.firebasedatabase.app",
+//     projectId: "onlinepharmacyreact",
+//     storageBucket: "onlinepharmacyreact.firebasestorage.app",
+//     messagingSenderId: "1027126627603",
+//     appId: "1:1027126627603:web:ac16ee5768221e17f99ee6",
+//     measurementId: "G-TXE599JF0B"
+//   };
 const firebaseConfig = {
-    apiKey: "AIzaSyDJ7YFne4QBWBcE45KUOmV8yIyHZP40rYI",
-    authDomain: "onlinepharmacyreact.firebaseapp.com",
-    databaseURL: "https://onlinepharmacyreact-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "onlinepharmacyreact",
-    storageBucket: "onlinepharmacyreact.firebasestorage.app",
-    messagingSenderId: "1027126627603",
-    appId: "1:1027126627603:web:ac16ee5768221e17f99ee6",
-    measurementId: "G-TXE599JF0B"
-  };
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+};
 
 const app = initializeApp(firebaseConfig);
 
